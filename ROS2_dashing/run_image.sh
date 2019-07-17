f6f774f91e7e9313a69e97a1b9233c822375b627
KobayashiRui <roboroborx782@gmail.com>
2019-07-18 03:17:27 +0900

    update

---
+++ ROS2_dashing/run_image.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+docker run -it \
+    --env="DISPLAY" \
+    --env="QT_X11_NO_MITSHM=1" \
+    --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
+    -v /home/kr/git/ROS_practice/ROS_data/:/ros_ws/src \
+    --runtime=nvidia \
+    ros2/robot \
+    bash
